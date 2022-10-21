f1c384ced7a26cc3f5082181715e14e37ecb0d49
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-10-21 11:03:06 +0000

    10月21日 - 20:00

--- main.py
+++ main.py
@@ -2,15 +2,25 @@
 import prob_calculator
 from unittest import main
 
+''' __init__
+hat = prob_calculator.Hat(blue=2, green=1)
+'''
+
+''' draw
+hat = prob_calculator.Hat(blue=2, green=2)
+hat.draw(3)
+'''
+
+''' Original Code
 prob_calculator.random.seed(95)
 hat = prob_calculator.Hat(blue=4, red=2, green=6)
 probability = prob_calculator.experiment(
     hat=hat,
-    expected_balls={"blue": 2,
-                    "red": 1},
+    expected_balls={"blue": 2, "red": 1},
     num_balls_drawn=4,
     num_experiments=3000)
 print("Probability:", probability)
+'''
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+#main(module='test_module', exit=False)
--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,24 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-
+    
+    def __init__(self, **args):
+        self.contents = list()
+        for key, val in args.items():
+            for i in range(val):
+                self.contents.append(key)
+    
+    def draw(self, quantity):
+        return_list = list()
+        if quantity > len(self.contents):
+            for i in range(len(self.contents)):
+                return_list.append(self.contents.pop(0))
+        else:
+            for i in range(quantity):
+                idx = random.choice(range(len(self.contents)))
+                return_list.append(self.contents[idx])
+                self.contents.pop(idx)
+        return return_list
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    //
